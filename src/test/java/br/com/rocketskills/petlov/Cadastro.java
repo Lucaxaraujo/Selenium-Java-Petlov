d5973c7403b45efd0d7dbd09a48933958fca506d
Lucaxaraujo <lucasm030@gmail.com>
2024-07-02 20:13:47 -0300

    Adding BeforeEach and AfterEach JUnit hoooks to isolate opening and closing session

--- src/test/java/br/com/rocketskills/petlov/Cadastro.java
+++ src/test/java/br/com/rocketskills/petlov/Cadastro.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.Duration;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,13 +18,23 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 class Cadastro {
 
+	WebDriver driver;
+
+	@BeforeEach
+	void starSession() {
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	};
+
+	@AfterEach
+	void finisSession() {
+		driver.close();
+	};
+
 	@Test
 	@DisplayName("Deve cadastrar um ponto de doação")
 	void createDonationPoint() {
-		//Opening the signup page
-		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-
+		// Opening the signup page
 		driver.get("https://petlov.vercel.app/signup");
 
 		WebElement title = driver.findElement(By.cssSelector("h1"));
@@ -32,7 +44,7 @@ class Cadastro {
 
 		assertEquals("Cadastro de ponto de doação", title.getText(), "Verificando o título do formulário");
 
-		//Filling the form to create a donation point
+		// Filling the form to create a donation point
 		WebElement donationPointName = driver.findElement(By.cssSelector("input[placeholder='Nome do ponto de doação']"));
 		donationPointName.sendKeys("Ponto Teste");
 
@@ -40,7 +52,7 @@ class Cadastro {
 		email.sendKeys("emailteste@gmail.com");
 
 		WebElement zipcode = driver.findElement(By.cssSelector("input[name='cep']"));
-		zipcode.sendKeys("12070740"); 
+		zipcode.sendKeys("12070740");
 
 		WebElement addressNumber = driver.findElement(By.cssSelector("input[name='addressNumber']"));
 		addressNumber.sendKeys("50");
@@ -52,16 +64,14 @@ class Cadastro {
 
 		driver.findElement(By.className("button-register")).click();
 
-		//Checking success page after filling the form and create a donation point
+		// Checking success page after filling the form and create a donation point
 		WebElement successPageText = driver.findElement(By.cssSelector("#success-page p"));
 
 		Wait<WebDriver> waitSuccessPageText = new WebDriverWait(driver, Duration.ofSeconds(2));
 		waitSuccessPageText.until(d -> successPageText.isDisplayed());
 
-		String text = "Seu ponto de doacão foi adicionado com sucesso. Juntos, podemos criar um mundo onde todos os animais recebam o amor e cuidado que merecem.";
+		String text = "Seu ponto de doação foi adicionado com sucesso. Juntos, podemos criar um mundo onde todos os animais recebam o amor e cuidado que merecem.";
 
 		assertEquals(text, successPageText.getText(), "Verificando a mensagem da página de sucesso");
-	
-		driver.close();
 	}
 }
