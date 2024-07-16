598fe384cd8c486692031807e121708f74b7fe18
Lucaxaraujo <lucasm030@gmail.com>
2024-07-15 22:41:10 -0300

    Implemented test data modeling

--- src/test/java/br/com/rocketskills/petlov/SignupSelenide.java
+++ src/test/java/br/com/rocketskills/petlov/SignupSelenide.java
@@ -8,28 +8,56 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
+class DonationPoint {
+	String name;
+	String email;
+	String zipCode;
+	Integer addressNumber;
+	String addressDetail;
+	String pets;
+
+	public DonationPoint(String name, String email, String zipCode, Integer addressNumber, String addressDetail,
+			String pets) {
+		this.name = name;
+		this.email = email;
+		this.zipCode = zipCode;
+		this.addressNumber = addressNumber;
+		this.addressDetail = addressDetail;
+		this.pets = pets;
+	};
+};
+
 class SignupSelenide {
 
 	@Test
 	@DisplayName("Deve cadastrar um ponto de doação")
 	void createDonationPoint() {
-		// Opening the signup page
+		//(pre condition)
+		DonationPoint point = new DonationPoint(
+			"Estação Pet", 
+			"estacaopet@hotmail.com", 
+			"12070740", 
+			118,
+			"Próximo à creche", 
+			"Cachorros"		
+		);
+		
+		// Opening the signup page 
 		open("https://petlov.vercel.app/signup");
 		$("h1").shouldHave(text("Cadastro de ponto de doação"));
-		
-		// Filling the form to create a donation point
-		$("input[placeholder='Nome do ponto de doação']").setValue("Estação Pet");
-		$("input[name='email']").setValue("estacaopet@hotmail.com");
-		$("input[name='cep']").setValue("12070740");
+
+		// Filling the form to create a donation point (action)
+		$("input[placeholder='Nome do ponto de doação']").setValue(point.name);
+		$("input[name='email']").setValue(point.email);
+		$("input[name='cep']").setValue(point.zipCode);
 		$("input[value='Buscar CEP']").click();
-		$("input[name='addressNumber']").setValue("118");
-		$("input[name='addressDetails']").setValue("Próximo à creche");
-		$(By.xpath("//span[text()=\"Cachorros\"]/..")).click();
+		$("input[name='addressNumber']").setValue(point.addressNumber.toString());
+		$("input[name='addressDetails']").setValue(point.addressDetail);
+		$(By.xpath("//span[text()=\"" +point.pets + "\"]/..")).click();
 		$(".button-register").click();
 
-		// Checking success page after filling the form and create a donation point
+		// Checking success page after filling the form and create a donation point (expected result)
 		String text = "Seu ponto de doação foi adicionado com sucesso. Juntos, podemos criar um mundo onde todos os animais recebam o amor e cuidado que merecem.";
-
-		$("#success-page p").shouldHave(text(text));		
+		$("#success-page p").shouldHave(text(text));
 	}
 }
