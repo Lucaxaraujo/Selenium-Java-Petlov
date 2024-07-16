26171e0ae39743b7f51c66638b6c24f9ce238200
Lucaxaraujo <lucasm030@gmail.com>
2024-07-15 22:53:50 -0300

    Refactored structure to reuse the submit form logic

--- src/test/java/br/com/rocketskills/petlov/SignupSelenide.java
+++ src/test/java/br/com/rocketskills/petlov/SignupSelenide.java
@@ -28,6 +28,16 @@ class DonationPoint {
 };
 
 class SignupSelenide {
+	private void submitForm(DonationPoint point) {
+		$("input[placeholder='Nome do ponto de doação']").setValue(point.name);
+		$("input[name='email']").setValue(point.email);
+		$("input[name='cep']").setValue(point.zipCode);
+		$("input[value='Buscar CEP']").click();
+		$("input[name='addressNumber']").setValue(point.addressNumber.toString());
+		$("input[name='addressDetails']").setValue(point.addressDetail);
+		$(By.xpath("//span[text()=\"" +point.pets + "\"]/..")).click();
+		$(".button-register").click();
+	};
 
 	@Test
 	@DisplayName("Deve cadastrar um ponto de doação")
@@ -41,20 +51,13 @@ class SignupSelenide {
 			"Próximo à creche", 
 			"Cachorros"		
 		);
-		
+
 		// Opening the signup page 
 		open("https://petlov.vercel.app/signup");
 		$("h1").shouldHave(text("Cadastro de ponto de doação"));
 
 		// Filling the form to create a donation point (action)
-		$("input[placeholder='Nome do ponto de doação']").setValue(point.name);
-		$("input[name='email']").setValue(point.email);
-		$("input[name='cep']").setValue(point.zipCode);
-		$("input[value='Buscar CEP']").click();
-		$("input[name='addressNumber']").setValue(point.addressNumber.toString());
-		$("input[name='addressDetails']").setValue(point.addressDetail);
-		$(By.xpath("//span[text()=\"" +point.pets + "\"]/..")).click();
-		$(".button-register").click();
+		submitForm(point);
 
 		// Checking success page after filling the form and create a donation point (expected result)
 		String text = "Seu ponto de doação foi adicionado com sucesso. Juntos, podemos criar um mundo onde todos os animais recebam o amor e cuidado que merecem.";
